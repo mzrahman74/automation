d2913c013298c1d7225676290e8ac057aa1e7628
Mohammad Rahman <mzrahman74@gmail.com>
2021-06-13 19:10:48 -0500

    cpypress test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,23 @@
 Cypress.Commands.add("openHomePage", () => {
   cy.visit("/");
 });
+
+Cypress.Commands.add('loginToApplication', ()=> {
+  const userCredentials = {
+    "user": {
+      "email": Cypress.env('username'),
+      "password": Cypress.env('password')
+    }
+  }
+  cy.request('POS', Cypress.env('apiUrl')+api/users/login, userCredentials)
+    .its('body').then (body => {
+      const token = body.user.token
+        cy.wrap(token).as('token')
+        cy.visit('/', {
+          OnBeforeLoad (win) {
+            win.localStorage.setTime('jwToken', token)
+          }
+        })
+      
+    })
+})
